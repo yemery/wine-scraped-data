3047ce72e341b2f38f1b616ce073d2acaf785e33
yemery <meryemajmani33@gmail.com>
2022-11-18 16:32:57 +0100

    -

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -4,7 +4,8 @@ import requests
 import numpy as np
 import time
 import requests
-
+import csv
+from termcolor import colored
 # wf=open('Urls/Url.txt',"r+")
 # lines=wf.readlines()
 # for i in lines:
@@ -12,47 +13,35 @@ import requests
 
 def get_wine_links():
     urlLinks=[]
-    wf=open('Urls/Url.txt',"r+")
+    wf=open('Urls/Url.txt',"r")
     lines=wf.readlines()
     for i in lines:
-        urlLinks.append(i.rstrip())
+        if i.strip()=="":
+            continue
+        else:
+            urlLinks.append(i.strip('\n'))
     return urlLinks
 
 
 def get_wine_info():
     products=[]
-
-
     links=get_wine_links()
-    # for i in links:
-    #         URL = f'{i}'
-    #         print(URL)
-    #         r = requests.get(URL)
-    #         soup=BeautifulSoup(r.content,'html5lib')
     for i in range(0,len(links)):
         URL = f"{links[i]}"
         # print(URL)
         r = requests.get(URL)
         soup=BeautifulSoup(r.content,'html5lib')
         # print(soup.prettify())
-        # print('***')
         image=soup.find('img', {"id": "product-main-image"})
         srcImg=image.attrs['src']
         # print(srcImg)
-
-        
-
         desc=soup.find('div', {"id": "product-description"})
-
         title=desc.find('h1').text
         # print(title)
-
         price=desc.find('span',{"class": "product-price"}).text
         # print(price)
-
         itemInfo=desc.find('div', {"id": "item-info"})
         ulInfo=desc.find('ul')
-
         # # print(itemInfo.prettify())
         list=[]
         for j in ulInfo.find_all('li'):
@@ -64,12 +53,10 @@ def get_wine_info():
                 # print(i.find_all('p'))
                 list.append(j.find_all('p')[1].text)
         # print(len(list))
-
         if(len(list)==6):
             # print(len(list))
             type, grape, country,region,abv,bottleSize = [i for i in list]
         # print(type, grape, country,region,abv,bottleSize)
-
         description=soup.find("div",{"class": "meta--text"}).text.strip()
         # print(description)
         keys=['id','image','title','price','type','grape','country','region','abv','bottlesize']
@@ -80,12 +67,33 @@ def get_wine_info():
             product[keys[k]]=values[k]
         products.append(product)
         time.sleep(2)
-        print(product)
+        # print(product)
     # print(products)
     return products
 
 def scraped_data_into_csv():
-    pass
+    products=get_wine_info()
+    # print(products)
+    with open("data.csv", 'w',encoding="utf-8") as csvfile: 
+        writer = csv.DictWriter(csvfile, fieldnames =['id','image','title','price','type','grape','country','region','abv','bottlesize'] ) 
+
+        writer.writeheader() 
+
+        # writing data rows 
+        writer.writerows(products) 
+        print(colored("added secc...\n",'blue'))
+
+    
+
+scraped_data_into_csv()
+
+# if __name__ == "__main__":
+#     start_time = time.time()
+#     print(colored("Start...\n",'blue'))
+
+#     scraped_data_into_csv()
+#     print(colored("\nDone :)","blue"))
+#     print(colored("--- %s seconds ---"%(time.time() - start_time),"yellow"))
 
 
 
@@ -98,14 +106,6 @@ def scraped_data_into_csv():
 
 
             
-
-  
-
-
-
-    
-
-get_wine_info()
 
     
     
--- Urls/test.py
+++ Urls/test.py
@@ -1,15 +1,162 @@
-# [{"name": "Felix Maina", "years": 21, "school": "Makerere"}]import json
-# # Data to be written
-# details = [{
-#         "name": "Felix Maina",
-#         "years": 21,
-#         "school": "Makerere"
-# }]
-# # Serializing JSON and writing JSON file
-# with open("data.json", "w") as file_object:
-#     json.dump(details, file_object)  # {"
+# # [{"name": "Felix Maina", "years": 21, "school": "Makerere"}]import json
+# # # Data to be written
+# # details = [{
+# #         "name": "Felix Maina",
+# #         "years": 21,
+# #         "school": "Makerere"
+# # }]
+# # # Serializing JSON and writing JSON file
+# # with open("data.json", "w") as file_object:
+# #     json.dump(details, file_object)  # {"
 
-wf=open('Urls/Url.txt',"r+")
-lines=wf.readlines()
-for i in lines:
-    print(i.rstrip())
+# # wf=open('Urls/Url.txt',"r+")
+# # lines=wf.readlines()
+# # for i in lines:
+# #     print(i.rstrip())
+
+# # importing the csv module
+# # import csv
+	
+# # # my data rows as dictionary objects
+# # mydict =[{'branch': 'COE', 'cgpa': '9.0', 'name': 'Nikhil', 'year': '2'},
+# # 		{'branch': 'COE', 'cgpa': '9.1', 'name': 'Sanchit', 'year': '2'},
+# # 		{'branch': 'IT', 'cgpa': '9.3', 'name': 'Aditya', 'year': '2'},
+# # 		{'branch': 'SE', 'cgpa': '9.5', 'name': 'Sagar', 'year': '1'},
+# # 		{'branch': 'MCE', 'cgpa': '7.8', 'name': 'Prateek', 'year': '3'},
+# # 		{'branch': 'EP', 'cgpa': '9.1', 'name': 'Sahil', 'year': '2'}]
+	
+# # # field names
+# # fields = ['name', 'branch', 'year', 'cgpa']
+	
+# # # name of csv file
+# # filename = "Csv/data.csv"
+	
+# # # writing to csv file
+# # with open(filename, 'w') as csvfile:
+# # 	# creating a csv dict writer object
+# # 	writer = csv.DictWriter(csvfile, fieldnames = fields)
+		
+# # 	# writing headers (field names)
+# # 	writer.writeheader()
+		
+# # 	# writing data rows
+# # 	writer.writerows(mydict)
+
+
+# from lxml import html
+# from bs4 import BeautifulSoup
+# import requests
+# import numpy as np
+# import time
+# import requests
+
+# # wf=open('Urls/Url.txt',"r+")
+# # lines=wf.readlines()
+# # for i in lines:
+# #     print(i.rstrip())
+
+# def get_wine_links():
+#     urlLinks=[]
+#     wf=open('Urls/Url.txt',"r+")
+#     lines=wf.readlines()
+#     for i in lines:
+#         urlLinks.append(i.rstrip())
+#     return urlLinks
+
+
+# def get_wine_info():
+#     products=[]
+
+
+#     links=get_wine_links()
+#     # for i in links:
+#     #         URL = f'{i}'
+#     #         print(URL)
+#     #         r = requests.get(URL)
+#     #         soup=BeautifulSoup(r.content,'html5lib')
+#     for i in range(0,len(links)):
+#         URL = f"{links[i]}"
+#         # print(URL)
+#         r = requests.get(URL)
+#         soup=BeautifulSoup(r.content,'html5lib')
+#         # print(soup.prettify())
+#         # print('***')
+#         image=soup.find('img', {"id": "product-main-image"})
+#         srcImg=image.attrs['src']
+#         # print(srcImg)
+
+        
+
+#         desc=soup.find('div', {"id": "product-description"})
+
+#         title=desc.find('h1').text
+#         # print(title)
+
+#         price=desc.find('span',{"class": "product-price"}).text
+#         # print(price)
+
+#         itemInfo=desc.find('div', {"id": "item-info"})
+#         ulInfo=desc.find('ul')
+
+#         # # print(itemInfo.prettify())
+#         list=[]
+#         for j in ulInfo.find_all('li'):
+#             p=j.find('p')
+#             # print(p.text)
+#             if (p.text in ['Type:',"Country:","Bottle Size:",'Region:',"ABV:",'Grape:']):
+#                 # print(p.text)
+#                 # print(i.find_all('p')[1].text)s
+#                 # print(i.find_all('p'))
+#                 list.append(j.find_all('p')[1].text)
+#         # print(len(list))
+
+#         if(len(list)==6):
+#             # print(len(list))
+#             type, grape, country,region,abv,bottleSize = [i for i in list]
+#         # print(type, grape, country,region,abv,bottleSize)
+
+#         description=soup.find("div",{"class": "meta--text"}).text.strip()
+#         # print(description)
+#         keys=['id','image','title','price','type','grape','country','region','abv','bottlesize']
+#         values=[i,srcImg,title,price,type,grape,country,region,abv,bottleSize]
+#         # print([1,srcImg,title,price,type,grape,country,region,abv,bottleSize])
+#         product={}
+#         for k in range(0,len(keys)):
+#             product[keys[k]]=values[k]
+#         products.append(product)
+#         time.sleep(2)
+#         print(product)
+#     # print(products)
+#     return products
+
+# def scraped_data_into_csv():
+#     pass
+
+
+
+
+
+
+
+    
+
+
+
+            
+
+  
+
+
+
+    
+
+# get_wine_info()
+
+    
+    
+
+
+
+
+
+
